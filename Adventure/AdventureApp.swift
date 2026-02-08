b32b5dd942bca5b6e139e8d43b45b08ffee80af7
Muhammad Jawad <jawadbqa@gmail.com>
2026-02-08 13:07:40 +0100

    Initial Commit

---
+++ Adventure/AdventureApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdventureApp.swift
+//  Adventure
+//
+//  Created by Muhammad Jawad on 2026-02-08.
+//
+
+import SwiftUI
+
+@main
+struct AdventureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
